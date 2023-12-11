f4f01d9a55316037a5905256491bcefafed7b0ea
ivan-burgio <ivan.24.burgio@gmail.com>
2023-12-11 16:29:59 -0300

    Resaltar la pagina actual

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,20 +1,33 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation, NavLink } from "react-router-dom";
 
 export default function Layout() {
-  return (
-    <div className="md:flex md:min-h-screen">
-        <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
-            <h2 className="text-4x font-black text-center text-white">Administrador de Clientes</h2>
+    const location = useLocation();
 
-            <nav className="mt-10">
-                <Link className="text-2xl block mt-2 hover:text-blue-300 text-white" to="/">Clientes</Link>
-                <Link className="text-2xl block mt-2 hover:text-blue-300 text-white" to="/clientes/nuevo">Nuevo Cliente</Link>
-            </nav>
-        </aside>
+    return (
+        <div className="md:flex md:min-h-screen">
+            <aside className="md:w-1/4 bg-blue-900 px-5 py-10">
+                <h2 className="text-4x font-black text-center text-white">Administrador de Clientes</h2>
 
-        <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
-            <Outlet />
-        </main>
-    </div>
-  )
+                <nav className="mt-10">
+                    <Link
+                        className={`${location.pathname === '/' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
+                        to="/"
+                    >
+                        Clientes
+                    </Link>
+
+                    <Link 
+                        className={`${location.pathname === '/clientes/nuevo' ? 'text-blue-300' : 'text-white'} text-2xl block mt-2 hover:text-blue-300`}
+                        to="/clientes/nuevo"
+                    >
+                        Nuevo Cliente
+                    </Link>
+                </nav>
+            </aside>
+
+            <main className="md:w-3/4 p-10 md:h-screen overflow-scroll">
+                <Outlet />
+            </main>
+        </div>
+    )
 }
