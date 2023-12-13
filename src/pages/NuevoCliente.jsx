7e431e04eb825f3e2a10f15b28f9c6d15e02ceee
ivan-burgio <ivan.24.burgio@gmail.com>
2023-12-13 16:45:36 -0300

    Primeros pasos creando el formulario de clientes

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 export default function NuevoCliente() {
+    const navigate = useNavigate();
+
     return (
         <div>
-            Nuevo cliente
+            <h1 className="font-black text-4xl text-blue-900">Nuevo Cliente</h1>
+            <p className="mt-3">
+                Completa los campos para registrar a un nuevo cliente.
+            </p>
+
+            <div className="flex justify-end">
+                <button 
+                    className="bg-blue-800 text-white px-3 py-1 font-bold uppercase"
+                    onClick={() => navigate(-1)}
+                >
+                    Volver
+                </button>
+            </div>
+
+            <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10">
+                <p>formulario aqui</p>
+            </div>
         </div>
-    )
+    );
 }
