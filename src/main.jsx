a485dfe231a7d48a1fc2b064858f4907a02e7cc5
ivan-burgio <ivan.24.burgio@gmail.com>
2023-12-14 14:42:52 -0300

    Creando un action para el formulario

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Layout from './components/Layout';
 
-import NuevoCliente from './pages/NuevoCliente';
+import NuevoCliente, { action as nuevoClienteAction } from './pages/NuevoCliente';
 import Index, { loader as clientesLoader } from './pages/Index';
 
 const router = createBrowserRouter([
@@ -20,7 +20,8 @@ const router = createBrowserRouter([
             },
             {
                 path: '/clientes/nuevo',
-                element: <NuevoCliente />
+                element: <NuevoCliente />,
+                action: nuevoClienteAction
             },
         ]
     }
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,5 +1,9 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Form } from "react-router-dom";
 import Formulario from "../components/Formulario";
+
+export function action() {
+    console.log('a');
+}
 
 export default function NuevoCliente() {
     const navigate = useNavigate();
@@ -21,7 +25,9 @@ export default function NuevoCliente() {
             </div>
 
             <div className="bg-white shadow rounded-md md:w-3/4 mx-auto px-5 py-10 mt-20">
-                <form>
+                <Form
+                    method="post"
+                >
                     <Formulario />
 
                     <input
@@ -29,7 +35,7 @@ export default function NuevoCliente() {
                         className="mt-5 w-full bg-blue-800 p-3 uppercase font-bold text-white text-lg"
                         value="Registrar Cliente"
                     />
-                </form>
+                </Form>
             </div>
         </div>
     );
